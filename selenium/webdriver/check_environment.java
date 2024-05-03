1b1d334f20052d3f49defcb1ac51a0ac7ee28c7e
coldzy <poseidonhiso@gmail.com>
2024-05-03 23:35:02 +0700

    add new topic 02

--- selenium/webdriver/check_environment.java
+++ selenium/webdriver/check_environment.java
@@ -27,6 +27,7 @@ public class check_environment {
         driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
         driver.manage().window().maximize();
         driver.get("https://www.facebook.com/");
+        driver.manage().window().maximize();
     }
 
     @Test
